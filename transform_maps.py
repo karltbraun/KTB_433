1703a0a2f91b47c9cd4f767d29ed9bae79e8988b
Karl Braun <karltbraun@gmail.com>
2023-07-12 08:21:30 -0700

    initial commit

---
+++ transform_maps.py
@@ -0,0 +1,8 @@
+"""transform_maps - provides dictionaries which define the mapping of various fields to more human-readable values"""
+
+model_map = {
+    "ACURITE-606TX": "ACURITE-606TX",
+    "INFACTORY-TH": "SMARTRO-SC91",
+}
+
+id_map = {"169": "SC91-A", "167": "SC91-B", "211": "SC91-C", "49": "ACRT-01"}
