63444bcef05d9868a52a601d67ade172e2a61ed1
Karl Braun <karltbraun@gmail.com>
2023-07-16 12:30:16 -0700

    Added HiveMQ credentials

--- mqtt_secrets.py
+++ mqtt_secrets.py
@@ -2,14 +2,17 @@
 
 # MQTT broker address/URL
 MQTT_BROKER_ADDRESS = "10.24.94.78"
+MQTT_BROKER_ADDRESS = "2f7fe884457d48daa446c05fccdf2cf4.s1.eu.hivemq.cloud"
 
 # MQTT broker TCP port
 MQTT_BROKER_PORT = 1883
 
 # MQTT broker username
+# MQTT_USERNAME = ""
 # MQTT_USERNAME = "karl"
-MQTT_USERNAME = ""
+MQTT_USERNAME = "ktbmes"  # HiveMQ
 
 # MQTT broker password
+# MQTT_PASSWORD = ""
 # MQTT_PASSWORD = "m00nmom"
-MQTT_PASSWORD = ""
+MQTT_PASSWORD = "KTBMESktbmes2023"  # HiveMQ
