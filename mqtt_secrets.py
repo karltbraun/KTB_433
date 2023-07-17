175b61613dc29722a7a556bbde5a83210fe7ee41
Karl Braun <karltbraun@gmail.com>
2023-07-16 17:42:07 -0700

    Set up with dictionary to more easily select a broker

--- mqtt_secrets.py
+++ mqtt_secrets.py
@@ -1,18 +1,44 @@
 # mqtt_secrets.py
 
+MQTT_BROKER = "MQTT-ORG"
+
+HIVE_MQTT_BROKER_ADDRESS = "broker.hivemq.com"
+
+MQTT_CONFIG = {
+    "MQTT-ORG": {
+        "MQTT_BROKER_ADDRESS": "test.mosquitto.org",
+        "MQTT_BROKER_PORT": 1883,
+        "MQTT_USERNAME": "",
+        "MQTT_PASSWORD": "",
+    },
+    "HIVEMQ": {
+        "MQTT_BROKER_ADDRESS": HIVE_MQTT_BROKER_ADDRESS,
+        "MQTT_BROKER_PORT": 8883,
+        "MQTT_USERNAME": "ktbmes",
+        "MQTT_PASSWORD": "KTBMESktbmes2023",
+    },
+    "KTB-IMAC-LINUX": {
+        "MQTT_BROKER_ADDRESS": "10.24.94.78",
+        "MQTT_BROKER_PORT": 1883,
+        "MQTT_USERNAME": "",
+        "MQTT_PASSWORD": "",
+    },
+    "VULTR-01": {
+        "MQTT_BROKER_ADDRESS": "0.0.0.0",
+        "MQTT_BROKER_PORT": 1883,
+        "MQTT_USERNAME": "",
+        "MQTT_PASSWORD": "",
+    },
+}
+
 # MQTT broker address/URL
-MQTT_BROKER_ADDRESS = "10.24.94.78"
-MQTT_BROKER_ADDRESS = "2f7fe884457d48daa446c05fccdf2cf4.s1.eu.hivemq.cloud"
+MQTT_BROKER_ADDRESS = MQTT_CONFIG[MQTT_BROKER]["MQTT_BROKER_ADDRESS"]
 
 # MQTT broker TCP port
-MQTT_BROKER_PORT = 1883
+MQTT_BROKER_PORT = MQTT_CONFIG[MQTT_BROKER]["MQTT_BROKER_PORT"]
 
 # MQTT broker username
-# MQTT_USERNAME = ""
-# MQTT_USERNAME = "karl"
-MQTT_USERNAME = "ktbmes"  # HiveMQ
+MQTT_USERNAME = MQTT_CONFIG[MQTT_BROKER]["MQTT_USERNAME"]
 
 # MQTT broker password
-# MQTT_PASSWORD = ""
-# MQTT_PASSWORD = "m00nmom"
-MQTT_PASSWORD = "KTBMESktbmes2023"  # HiveMQ
+MQTT_PASSWORD = MQTT_CONFIG[MQTT_BROKER]["MQTT_PASSWORD"]
